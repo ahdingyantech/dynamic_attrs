5333e015ce012f9e8afac4e246005323728e52f3
Kaid <kaid@kaid.me>
2013-03-14 18:11:09 +0800

    made field accessible

--- lib/dynamic_attrs.rb
+++ lib/dynamic_attrs.rb
@@ -5,6 +5,7 @@ require 'dynamic_attrs/da_type'
 Dir[File.dirname(__FILE__) + '/dynamic_attrs/da_type/*.rb'].each {|f| require f}
 
 class DynamicAttr < ActiveRecord::Base
+  attr_accessible :field
   belongs_to :owner, polymorphic: true
 
   class TypeMismatch < Exception; end
